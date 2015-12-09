7848a52fdba3f002bf5f55e1d92947c90b881b26
Jaya Vijayen <Jaya Vijayen>
2015-12-08 21:48:28 -0800

    all hackbright work

--- Project/expresslane2014.py
+++ Project/expresslane2014.py
@@ -1,5 +1,11 @@
-import csv
+# Which month has the most number of cars?
+# Which week has the most number of cars?
+# Which day has the most number of cars?
+# Which hour has the most number of cars?
+# Which direction (237 Westbound or 237 Eastbound) has the most number of cars?
 
+import csv
+# Open the csv file
 with open('ExpressLanesTrafficWithTolls-2014.csv', 'rb') as csvfile:
      trafficDataRaw = csv.reader(csvfile, delimiter=',', quotechar='|')
      trafficDataRaw.next()
@@ -10,6 +16,15 @@ with open('ExpressLanesTrafficWithTolls-2014.csv', 'rb') as csvfile:
      		row[3] = int(row[3])
      	if row[2] != '' or row[2] != "HOV ONLY" or row[2] != " ":
      		row[2] = float(row[2])
-     	#else row[2] == '' or row[2] == " " or row[2] == "HOV ONLY" or row[3] == '' or row[4] == '':
-             #  row = int(0)
      	print row
+
+# Calculate cars monthly
+          traffic = trafficDataRaw
+          split_traffic = traffic.split(",")
+          split_datetime = datetime.split(" ") 
+          cars = int(split_traffic[3])
+          date = split_traffic[0].split(" ")[0]
+          def calculateCarsMonth(cars, date):
+               return cars * datetime 
+
+          calculateCarsMonth(cars,date)
